4b135978cba876dbe9f679905fe0a46bcd3f9369
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2018-12-11 11:27:45 +0100

    DeepQ Learning

--- DeepQNetwork.py
+++ DeepQNetwork.py
@@ -30,13 +30,39 @@ model.add(tf.keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu'))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128, activation='relu'))
 model.add(tf.keras.layers.Dense(env.action_space.n, activation='softmax'))
-model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=learning_rate))
+model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=learning_rate), metrics=['accuracy'])
+
+
 
 max_games =2
 epsilon = 0.9
 gamma=0.8
 
+
+
 memory = deque()
+
+##train net
+def train_model(memory):
+    inputs = np.zeros([len(memory),210,160,1])
+    targets = np.zeros([len(memory),env.action_space.n])
+    iter = 0
+    print("Memory size = " + str(len(memory)))
+    print("Prepare inputs:")
+    for mem_element  in memory:
+        #print(iter)
+        #print(mem_element)
+        state, action, state_new, reward, done = mem_element
+        futureReward = np.max(model.predict(np.reshape(state_new,[1,210,160,1])))
+        target = reward + gamma*futureReward
+        inputs[iter] = np.reshape(state,[210,160,1])
+        targets[iter] = target
+        iter+=1
+    print("Train model:")
+    model.fit(inputs,targets,epochs=1)
+    global epsilon
+    epsilon =epsilon*.9
+
 ##Test play - fill memory
 for g in range(0,max_games):
     done = False
@@ -52,26 +78,12 @@ for g in range(0,max_games):
         state_new, reward, done, info = env.step(action)
         state_new = process_observation(state_new,[210,160])
         memory.append((state, action, state_new, reward, done))
+        if len(memory) > 10:
+            train_model(memory)
+            memory.clear()
         if done:
             print ("die "+str(g))
 
-##train net
-inputs = np.zeros([len(memory),210,160,1])
-targets = np.zeros([len(memory),env.action_space.n])
-iter = 0
-print("Memory size = " + str(len(memory)))
-print("Prepare inputs:")
-for mem_element  in memory:
-    #print(iter)
-    #print(mem_element)
-    state, action, state_new, reward, done = mem_element
-    futureReward = np.max(model.predict(np.reshape(state_new,[1,210,160,1])))
-    target = reward + gamma*futureReward
-    inputs[iter] = np.reshape(state,[210,160,1])
-    targets[iter] = target
-    iter+=1
-print("Train model:")
-model.fit(inputs,targets,epochs=1)
-epsilon =epsilon*.9
+
 
 
